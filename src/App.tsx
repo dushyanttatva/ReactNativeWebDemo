b3605c973a1a1d44fec139a23b6b4910416cf0a3
Dushyant Gadhiya <dushyant.gadhiya@tatvasoft.com>
2025-04-23 10:45:49 +0530

    rn web movie finder

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,17 @@
-import React, {useState} from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  Pressable,
-  useWindowDimensions,
-} from 'react-native';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import MovieDetailsPage from './MovieDetailsPage';
 
-export default function App() {
-  const {height} = useWindowDimensions();
-  const [number, setNumber] = useState(0);
-
-  function handlePress() {
-    setNumber(Number(Math.random() * 10000) % 100);
-  }
+function App() {
 
   return (
-    <View style={[styles.container, {height}, StyleSheet.absoluteFill]}>
-      <Text>Random number: {number}</Text>
-      <View style={styles.br} />
-      <Pressable
-        style={({pressed}) => [
-          {
-            opacity: pressed ? 0.7 : 1,
-          },
-          styles.btn,
-        ]}
-        onPress={handlePress}>
-        <Text style={styles.btnText}>Generate a number</Text>
-      </Pressable>
-    </View>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:id" element={<MovieDetailsPage />} />
+      </Routes>
+    </Router>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  br: {
-    height: 12,
-  },
-  btn: {
-    backgroundColor: '#222',
-    padding: 10,
-  },
-  btnText: {
-    color: '#fff',
-  },
-});
+export default App
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import {AppRegistry} from 'react-native';
+import './styles.css';
 import App from './App';
 
 AppRegistry.registerComponent('App', () => App);
